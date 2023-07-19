6bff636036060fb8a149a17701d37b33d619ee89
Tatwamasi Mishra <tatwamasi01@gmail.com>
2023-07-19 18:13:39 +0530

    integrated morgan for http req logging

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(express.json());
 
