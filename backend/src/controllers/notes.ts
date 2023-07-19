2c53c974cb145076611554372011d771251d9da3
Tatwamasi Mishra <tatwamasi01@gmail.com>
2023-07-19 17:31:48 +0530

    getNote successful

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -12,6 +12,17 @@ export const getNotes : RequestHandler = async (req, res, next) => {
     }
 };
 
+export const getNote: RequestHandler = async (req, res, next) => {
+    const noteId = req.params.noteId;
+    try {
+        const note = await NoteModel.findById(noteId).exec();
+        res.status(200).json(note);
+    } catch (error) {
+        next(error);
+    }
+
+};
+
 export const createNote: RequestHandler = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -7,6 +7,8 @@ const router = express.Router();
 // creating an endpoint for a HTTP get request
 router.get("/", NotesController.getNotes);
 
+router.get("/:noteId", NotesController.getNote);
+
 router.post("/", NotesController.createNote);
 
 export default router;
