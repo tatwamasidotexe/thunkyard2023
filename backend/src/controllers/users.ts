4d983f57204a519e43779337e4b9e708015481d5
Tatwamasi Mishra <tatwamasi01@gmail.com>
2023-08-21 20:39:06 +0530

    login session cookie setup successful

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -42,9 +42,43 @@ export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = asy
             password: passwordHashed,
         });
 
+        req.session.userId = newUser._id;
+
         res.status(201).json(newUser);
     } catch (error) {
         next(error);
     }
 
+};
+
+interface LoginBody {
+    username?: string,
+    password?: string,
 }
+
+export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        if (!username || !password) {
+            throw createHttpError(400, "Parameters are missing broski.");
+        }
+
+        const user = await UserModel.findOne({username: username}).select("+password +email").exec();
+
+        if(!user) {
+            throw createHttpError(401, "Invalid credentials!");
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if(!passwordMatch) {
+            throw createHttpError(401, "Invalid credentials!");
+        }
+
+        req.session.userId = user._id;
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post("/signup", UserController.signUp);
 
+router.post("/login", UserController.login);
+
 export default router;
