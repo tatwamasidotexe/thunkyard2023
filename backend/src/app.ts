38405262fba30718624af198414c24012d468091
Tatwamasi Mishra <tatwamasi01@gmail.com>
2023-09-12 02:06:32 +0530

    small auth middleware added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import createHttpError, { isHttpError } from "http-errors";
 import session from "express-session";
 import env from "./util/validateEnv";
 import MongoStore from "connect-mongo";
+import { requiresAuth } from "./middleware/auth";
 
 const app = express();
 
@@ -27,7 +28,7 @@ app.use(session({
     }),
 }));
 
-app.use("/api/notes", notesRoutes);
+app.use("/api/notes", requiresAuth, notesRoutes);
 
 app.use("/api/users", userRoutes);
 
