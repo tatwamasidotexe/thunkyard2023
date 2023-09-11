4870b7eb2ace8ccde4f307070eccfc9792e0ad4b
Tatwamasi Mishra <tatwamasi01@gmail.com>
2023-09-12 01:22:29 +0530

    login/logout successful

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,33 +3,69 @@ import LoginModal from './components/LoginModal';
 import NavBar from './components/NavBar';
 import SignUpModal from './components/SignUpModal';
 import styles from "./styles/NotesPage.module.css";
+import { useState, useEffect } from 'react';
+import { User } from './models/user';
+import * as NotesApi from "./network/notes_api";
+import NotesPageLoggedInView from './components/NotesPageLoggedInView';
+import NotesPageLoggedOutView from './components/NotesPageLoggedOutView';
 
 function App() {
 
+  const [loggedInUser, setLoggedInUser] = useState<User|null>(null);
+
+  const [showSignUpModal, setShowSignUpModal] = useState(false);
+  const [showLogInpModal, setShowLogInModal] = useState(false);
+
+  useEffect(() => {
+    async function fetchLoggedInUser() {
+      try {
+        const user = await NotesApi.getLoggedInUser();
+        setLoggedInUser(user);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchLoggedInUser();
+  }, []);
+
+ 
   return (
     <div>
       <NavBar
-        loggedInUser={null}
-        onLoginClicked={() => { }}
-        onSignUpClicked={() => { }}
-        onLogoutSuccessful={() => { }}
+        loggedInUser={loggedInUser}
+        onLoginClicked={() => setShowLogInModal(true)}
+        onSignUpClicked={() => setShowSignUpModal(true)}
+        onLogoutSuccessful={() => setLoggedInUser(null)}
       />
       <Container className={styles.notesPage}>
+        <>
         {
-          false &&
-          <SignUpModal
-            onDismiss={() => { }}
-            onSignUpSuccessful={() => { }}
-          />
+          loggedInUser
+          ? <NotesPageLoggedInView/>
+          : <NotesPageLoggedOutView/>
         }
-        {
-          false &&
-          <LoginModal
-            onDismiss={() => { }}
-            onLoginSuccessful={() => { }}
-          />
-        }
+        </>
       </Container>
+      {
+          showSignUpModal &&
+          <SignUpModal
+            onDismiss={() => setShowSignUpModal(false)}
+            onSignUpSuccessful={(user) => {
+              setLoggedInUser(user);
+              setShowSignUpModal(false);
+            }}
+          />
+        }
+        {
+          showLogInpModal &&
+          <LoginModal
+            onDismiss={() => setShowLogInModal(false)}
+            onLoginSuccessful={(user) => {
+              setLoggedInUser(user);
+              setShowLogInModal(false);
+            }}
+          />
+        }
     </div>
   );
 }
